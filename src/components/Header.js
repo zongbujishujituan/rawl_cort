ddcad70674d7357fe1fbb304fd94a31bc7aa8cd2
guwen <zongbujishujituan@gmail.com>
2020-11-08 23:13:06 +0900

    test

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,19 +2,23 @@ import PropTypes from 'prop-types'
 import React from 'react'
 
 const Header = props => (
+  
   <header id="header" style={props.timeout ? { display: 'none' } : {}}>
     <div className="logo">
-      <span className="icon fa-diamond"></span>
+      <span className="icon fa-hand-rock-o"></span>
     </div>
     <div className="content">
       <div className="inner">
-        <h1>Dimension</h1>
+        <h1>RAWL CORT GYM</h1>
         <p>
-          A fully responsive site template designed by{' '}
-          <a href="https://html5up.net">HTML5 UP</a> and released
+          <h2>2020/11/1~ released</h2>
           <br />
-          for free under the{' '}
-          <a href="https://html5up.net/license">Creative Commons</a> license.
+          <h3>完全プライベートジムを自分だけの空間で</h3>
+          <a>マンツーマン個別トレーニング(1h ¥5,500~)</a><br />
+          <a>プライベートジム使い放題(month ¥11,000~)</a><br /><br />
+          <h3>周りを気にせずボディメイク</h3>
+          ・<a>友人・家族とのトレーニング</a><br />
+          ・<a>パーソナル指導の場所として</a><br />
         </p>
       </div>
     </div>
@@ -26,7 +30,7 @@ const Header = props => (
               props.onOpenArticle('intro')
             }}
           >
-            Intro
+            rc gym
           </button>
         </li>
         <li>
@@ -35,7 +39,7 @@ const Header = props => (
               props.onOpenArticle('work')
             }}
           >
-            Work
+            personal training
           </button>
         </li>
         <li>
@@ -44,7 +48,7 @@ const Header = props => (
               props.onOpenArticle('about')
             }}
           >
-            About
+            private (rental)
           </button>
         </li>
         <li>
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,12 @@ import React from 'react'
 import pic01 from '../images/pic01.jpg'
 import pic02 from '../images/pic02.jpg'
 import pic03 from '../images/pic03.jpg'
+import pic07 from '../images/pic07.jpg'
+import pic11 from '../images/pic11.jpg'
+import pic12 from '../images/pic12.jpg'
+import pic14 from '../images/pic14.jpg'
+import pic15 from '../images/pic15.1.jpg'
+import pic16 from '../images/pic16.jpg'
 
 class Main extends React.Component {
   render() {
@@ -28,17 +34,21 @@ class Main extends React.Component {
           }`}
           style={{ display: 'none' }}
         >
-          <h2 className="major">Intro</h2>
+          <h2 className="major">rc gym</h2>
+          <h3>RAWL CORT GYM (ラウルコートジム)通称RC GYM</h3>
           <span className="image main">
-            <img src={pic01} alt="" />
+            <img src={pic15} alt="" />
           </span>
           <p>
-            Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin
-            aliquam facilisis ante interdum congue. Integer mollis, nisl amet
-            convallis, porttitor magna ullamcorper, amet egestas mauris. Ut
-            magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas.
-            By the way, check out my <a href="#work">awesome work</a>.
-          </p>
+           様々な方にボディーメイクを楽しんで頂きたい!<br />
+           そんな気持ちから埼玉では珍しいレンタルスペース<a>RC GYM</a>がNew open!!<br />
+           ・パーソナルトレーニング１〜３ヶ月コース<br />
+           ・コンテスト出場コース/単発コース<br />
+           ・ジムスペース月利用 １ヶ月 ¥11,000〜<br /><br />
+           個人利用・フリーのトレーナーさんの指導場所利用も可能。<br />
+           他にも様々なプランがございます。<br />
+           お客様に最適なプランを提案いたしますので是非お気軽にご相談下さい。<br />
+           RC GYMオリジナルブランド<a href="#rawl_cort_url">RAWL CORT</a>もECサイトにて販売しております。</p>  
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
             dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora
@@ -62,15 +72,31 @@ class Main extends React.Component {
           }`}
           style={{ display: 'none' }}
         >
-          <h2 className="major">Work</h2>
+          <h2 className="major">Personal</h2>
+          <h3>トレーナー紹介</h3>
           <span className="image main">
-            <img src={pic02} alt="" />
+            <img src={pic14} alt="" />
+            <img src={pic11} alt="" />
           </span>
           <p>
-            Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu,
-            at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent
-            urna nisi, fringila lorem et vehicula lacinia quam. Integer
-            sollicitudin mauris nec lorem luctus ultrices.
+          プロフィール<br /><br />
+          ベタンコールト 光　日本とチリのハーフ<br /><br />
+          私はトレーニングを始めるまで178cmと比較的高身長ながら、52kgしかない、
+          かなりの痩せ型でした。洋服をかっこよく着こなしたい、そんな気持ちから<br />
+          ボディーメイクを始め、冬(バルクアップ期間)は80kg超。夏(減量期間)は60kg<br />後半〜70kg
+          といったように食事や運動を上手に組み合わせ無理なく、体重・体型をコントロールできるようになり
+          2020年に開催された身体の美しさを競うコンテスト<a>SMMER STYLE AWARD 2020</a>では
+          <a>オーバーオールを含め二冠</a>を勝ち取ることができました。
+          ただ痩せるだけではなくしっかりと筋肉を残しつつ
+          女性は綺麗なくびれ、ヒップアップ。男性はたくましく。<br />
+          マンツーマンだからこそ皆様の要望に細かくお応えできます。<br />
+          男性・女性、体重を増やしたい方・ダイエットしたい方も...目的は問いません。
+          是非私に、皆さんが輝くためのお手伝いをさせて下さい！<br />
+          一緒に一歩を踏み出しましょう！！<br /><br />
+          最新コンテスト出場結果<br />
+          Summer Style Award 2020<br />
+          Stylish Gyu Tall crass 1位<br />
+          Styilish Guy Overall 1位<br />
           </p>
           <p>
             Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
@@ -91,18 +117,30 @@ class Main extends React.Component {
           }`}
           style={{ display: 'none' }}
         >
-          <h2 className="major">About</h2>
+          <h2 className="major">Private</h2>
+          <h3>RC GYMではプライベート利用の月額会員様を順次募集しております。</h3>
           <span className="image main">
-            <img src={pic03} alt="" />
+            <img src={pic07} alt="" />
           </span>
           <p>
-            Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent
-            eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam
-            erat volutpat. Praesent urna nisi, fringila lorem et vehicula
-            lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.
-            Aliquam libero et malesuada fames ac ante ipsum primis in faucibus.
-            Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit
-            amet.
+            <h3>特徴</h3>
+            24時間利用可能・完全予約制<br />
+            1日1時間・1ヶ月 ¥11,000　ご友人、ご家族も同伴可<br /><br />
+            <h3>このような方にオススメです。</h3>
+            ・ジムのごちゃごちゃした雰囲気が苦手。待ち時間が勿体無いとお思いの方
+            RC GYMは完全事前予約制のプライベート空間です。<br />
+            その為周りの目も気にせず気軽に、そして待ち時間もなく効率よく<br />
+            トレーニングできます！<br /><br />
+            <h3>お忙しい方の時短トレーニングに最適です。</h3>
+            ・ご友人・ご家族とトレーニングしたい方
+            お１人ではもちろん、<br />
+            友人・ご家族もご同伴でご利用いただけます。<br />
+            *追加料金はありませんが会員様が必ずご同伴ください。<br /><br />
+            ・パーソナルトレーニングと併用したい方<br />
+            専属のパーソナルトレーナーがいつでもご対応いたします。
+            単発¥5,500〜
+            中々成長が見られない。フォームを見て欲しい。種目を教えてほしい。など
+            お好きな時にお気軽にご相談ください。
           </p>
           {close}
         </article>
